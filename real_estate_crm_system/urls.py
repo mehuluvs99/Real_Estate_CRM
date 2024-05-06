46356db6a9df9c6a07a6e52d7b3f4b008d52484a
anonymous7 <51238292+mehuluvs99@users.noreply.github.com>
2024-05-06 23:24:49 +0530

    update

--- real_estate_crm_system/urls.py
+++ real_estate_crm_system/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('users/', views.user_list, name='users'),
     path('add_inquiry/', views.add_inquiry, name='add_inquiry'),
     path('account/', views.accounts, name='account'),
-    path('upload_inquiry/', views.upload_file, name='upload_inquiry'),
+    path('upload_account/', views.upload_account_file, name='upload_account'),
+    path('upload_inquiry/', views.upload_inquiry_file, name='upload_inquiry'),
     path('payment/', views.payment, name='payment'),
     path('agent/', views.agent, name='agent'),
+    path('agent_data/', views.agent_data, name='agent_data'),
 ]
--- real_estate_crm_system/views.py
+++ real_estate_crm_system/views.py
@@ -86,6 +86,24 @@ def add_inquiry(request):
     return render(request, 'add_inquiry.html', {'form': form})
 
 
+def account_data(request):
+    account_records = Accounts.objects.all()
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect("home")
+        else:
+            messages.success(request, "There was an Error, Please try again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'account_records': account_records})
+
+
 def accounts(request):
     if request.method == 'POST':
         account = AccountsForm(request.POST)
@@ -120,6 +138,24 @@ def agent(request):
         form = AgentForm()
 
     return render(request, 'agent.html', {'form': form})
+
+def agent_data(request):
+    agent_records = Agents.objects.all()
+    print(agent_records)
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect("home")
+        else:
+            messages.success(request, "There was an Error, Please try again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'agent_records': agent_records})
 
 
 # def upload_file(request):
@@ -161,7 +197,7 @@ def agent(request):
 #     return render(request, 'upload_inquiry.html')  # Render the form for file upload
 
 
-def upload_file(request):
+def upload_inquiry_file(request):
     if request.method == 'POST':
         file = request.FILES['file_upload']
         print("Done")
@@ -268,3 +304,112 @@ def upload_file(request):
 
             return redirect('home')  # Redirect to a page after successful upload
     return render(request, 'upload_inquiry.html')
+
+
+def upload_account_file(request):
+    if request.method == 'POST':
+        file = request.FILES['file_upload']
+        print("Done")
+
+        if file.name.endswith('.xlsx'):
+            # Process the uploaded XLSX file using pandas
+            df = pd.read_excel(file)
+
+            for index, row in df.iterrows():
+                for field in ['inquiry_type', 'inquiry_stage', 'interested_project', 'selected_unit', 'assign_to', 'fields']:
+                    if pd.isna(row[field]):
+                        row[field] = None
+
+                follow_up_date = None
+                next_follow_up_date = None
+                converted_date = None
+
+                inquiry_type_instance = None
+                inquiry_stage_instance = None
+                interested_project_instance = None
+                selected_unit_instance = None
+                assign_to_instance = None
+                fields_instance = None
+
+                if pd.notna(row['follow_up_date']):
+                    follow_up_date_str = row['follow_up_date']
+                    # print(follow_up_date)
+                    # follow_up_date = datetime(follow_up_date_str, "%d/%m/%Y %H:%M:%S")
+                else:
+                    follow_up_date = None
+
+                if pd.notna(row['next_follow_up_date']):
+                    next_follow_up_date_str = row['next_follow_up_date']
+                    # next_follow_up_date = datetime(next_follow_up_date_str, "%d/%m/%Y %H:%M:%S")
+                else:
+                    next_follow_up_date = None
+
+                if pd.notna(row['converted_date']):
+                    converted_date_str = str(row['converted_date'])  # Assuming the date is stored as a string
+                    # converted_date = datetime(converted_date_str, "%d/%m/%Y %H:%M:%S")
+                else:
+                    converted_date = None
+
+                if row['inquiry_type'] is not None:
+                    try:
+                        inquiry_type_instance = Inquiry_Type.objects.get(inquiry_type=row['inquiry_type'])
+                    except Inquiry_Type.DoesNotExist:
+                        print(f"Inquiry_Type '{row['inquiry_type']}' does not exist.")
+
+                if row['inquiry_stage'] is not None:
+                    try:
+                        inquiry_stage_instance = Inquiry_Stage.objects.get(inquiry_stage=row['inquiry_stage'])
+                    except Inquiry_Stage.DoesNotExist:
+                        print(f"Inquiry_Stage '{row['inquiry_stage']}' does not exist.")
+
+                if row['interested_project'] is not None:
+                    try:
+                        interested_project_instance = Project_Name.objects.get(projectname=row['interested_project'])
+                    except Project_Name.DoesNotExist:
+                        print(f"Project_Name '{row['interested_project']}' does not exist.")
+
+                if row['selected_unit'] is not None:
+                    try:
+                        selected_unit_instance = Selected_Unit.objects.get(selected_unit=row['selected_unit'])
+                    except Selected_Unit.DoesNotExist:
+                        print(f"Selected_Unit '{row['selected_unit']}' does not exist.")
+
+                if row['assign_to'] is not None:
+                    try:
+                        assign_to_instance = Assign_To.objects.get(assign_to=row['assign_to'])
+                    except Assign_To.DoesNotExist:
+                        print(f"Assign_To '{row['assign_to']}' does not exist.")
+
+                if row['fields'] is not None:
+                    try:
+                        fields_instance = Fields.objects.get(fields=row['fields'])
+                    except Fields.DoesNotExist:
+                        print(f"Fields '{row['fields']}' does not exist.")
+
+                Add_Inquiry.objects.create(
+                    created_date=row['created_date'],
+                    first_name=row['first_name'],
+                    middle_name=row['middle_name'],
+                    last_name=row['last_name'],
+                    mobile_number=row['mobile_number'],
+                    whatsapp_mobile_number=row['whatsapp_mobile_number'],
+                    email_id=row['email_id'],
+                    address=row['address'],
+                    area=row['area'],
+                    city=row['city'],
+                    state=row['state'],
+                    country=row['country'],
+                    inquiry_type=inquiry_type_instance,
+                    inquiry_stage=inquiry_stage_instance,
+                    interested_project=interested_project_instance,
+                    selected_unit=selected_unit_instance,
+                    follow_up_date=follow_up_date,
+                    remarks=row['remarks'],
+                    next_follow_up_date=next_follow_up_date,
+                    converted_date=converted_date,
+                    assign_to=assign_to_instance,
+                    fields=fields_instance,
+                )
+
+            return redirect('home')  # Redirect to a page after successful upload
+    return render(request, 'upload_account.html')
